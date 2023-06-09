b6ef88a21fcbacd62d1c48629f2b40d9887423c4
Niklas Alexander Shern <90867839+nshern@users.noreply.github.com>
2023-06-09 13:32:56 +0200

    added fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,26 @@
+set -g fish_greeting
+
+if status is-interactive
+    fish_vi_key_bindings
+end
+
+# Keybindings
+bind -M insert \cf accept-autosuggestion
+bind -M insert \cr forward-word
+# bind -M insert \cg forward-char 
+
+# Abbreviations
+abbr -a gs 'git status'
+abbr -a vim nvim
+abbr -a venv 'source .venv/bin/activate.fish'
+abbr -a lg lazygit
+abbr -a clx 'clx -n'
+abbr -a ddgr "ddgr --reg "da-dk" --reverse"
+abbr -a py python
+abbr -a tt 'watson report -dcG | grep "Total: " | sed "s/Total: //"'
+abbr -a cls clear
+
+# Load Environment Variables
+source ~/.config/fish/env.fish
+
+starship init fish | source
---
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/nshern/.local/share/omf/themes/sashimi/fish_prompt.fish
