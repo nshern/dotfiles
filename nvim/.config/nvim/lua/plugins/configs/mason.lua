0f968e786f5deaf7026dc6f5949078bdc0c623f6
Niklas Alexander Shern <test@test.test>
2023-06-12 14:51:27 +0200

    .

--- nvim/.config/nvim/lua/plugins/configs/mason.lua
+++ nvim/.config/nvim/lua/plugins/configs/mason.lua
@@ -1,5 +1,5 @@
 local options = {
-  ensure_installed = { "lua-language-server", "pyright" }, -- not an option from mason.nvim
+  ensure_installed = { "lua-language-server" }, -- not an option from mason.nvim
 
   PATH = "skip",
 
--- nvim/.config/nvim/lua/plugins/configs/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,5 +1,5 @@
 local options = {
-  ensure_installed = { "lua", "python", "markdown", "html" },
+  ensure_installed = { "lua" },
 
   highlight = {
     enable = true,
