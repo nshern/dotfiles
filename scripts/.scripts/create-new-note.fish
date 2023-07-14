b3789c49553d34828cb6f394f623814db55d43ac
Niklas Alexander Shern <test@test.test>
2023-07-14 12:45:10 +0200

    feat: add yaml front matter to 'create note' script

--- scripts/.scripts/create-new-note.fish
+++ scripts/.scripts/create-new-note.fish
@@ -1,3 +1,17 @@
-#!/usr/bin/env fish
+#!/bin/bash
 
-nvim "notes/"(date "+%Y%m%d%H%M%S.md")
+# nvim "notes/"(date "+%Y%m%d%H%M%S.md")
+
+current_date=$(date "+%Y-%m-%d")
+current_date_and_time=$(date "+%Y%m%d%H%M%S")
+
+echo "---
+title: '$current_date_and_time'
+author: "Niklas Alexander Shern"
+date: '$current_date'
+lang: "da-DK"
+tags:
+---
+" > $current_date_and_time.md
+
+hx $current_date_and_time.md
