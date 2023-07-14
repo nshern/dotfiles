6ffd18b2b63d7067bf45a37d436829877f262290
Niklas Alexander Shern <test@test.test>
2023-07-14 22:33:02 +0200

    feat: add filename arg to create note script

--- scripts/.scripts/create-new-note.fish
+++ scripts/.scripts/create-new-note.fish
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # nvim "notes/"(date "+%Y%m%d%H%M%S.md")
-
+title=${1:-$(date "+%Y%m%d%H%M%S")}
 current_date=$(date "+%Y-%m-%d")
-current_date_and_time=$(date "+%Y%m%d%H%M%S")
+#current_date_and_time=$(date "+%Y%m%d%H%M%S")
 
 echo "---
-title: '$current_date_and_time'
+title: '$title'
 author: "Niklas Alexander Shern"
 date: '$current_date'
 lang: "da-DK"
 tags:
 ---
-" > $current_date_and_time.md
+" > ~/notes/$title.md
 
-hx ~/notes/$current_date_and_time.md
+hx ~/notes/$title.md
