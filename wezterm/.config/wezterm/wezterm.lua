044b434e235b3ffae718d148084f9cdb02b23e5c
Niklas Alexander Shern <nashern@protonmail.com>
2023-11-03 22:25:55 +0100

    switched to rose pine moon

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,8 @@ local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
 local config = {}
+local colors = require('lua/rose-pine-moon').colors()
+local window_frame = require('lua/rose-pine-moon').window_frame()
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
@@ -11,12 +13,19 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-config.color_scheme = 'Everforest Dark (Gogh)'
+-- config.color_scheme = 'Everforest Dark (Gogh)'
+-- config.color_scheme = 'Darcula (base16)'
+-- config.color_scheme = 'rose-pine-moon'
+
+
 
 -- For example, changing the color scheme:
+-- config.color_scheme = 'rose-pine-moon'
+config.colors = colors
+config.window_frame = window_frame
 config.native_macos_fullscreen_mode = true
 config.adjust_window_size_when_changing_font_size = false
-config.tab_bar_at_bottom = true
+config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
