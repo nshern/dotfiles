5ffd195ec42eb01f9e98dc44adf6882d63b9718a
Niklas Alexander Shern <nashern@protonmail.com>
2023-11-04 12:33:07 +0100

    .

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,23 +1,21 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
-local colors = require('lua/rose-pine-moon').colors()
-local window_frame = require('lua/rose-pine-moon').window_frame()
+local colors = require("lua/rose-pine-moon").colors()
+local window_frame = require("lua/rose-pine-moon").window_frame()
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 -- config.color_scheme = 'Everforest Dark (Gogh)'
 -- config.color_scheme = 'Darcula (base16)'
 -- config.color_scheme = 'rose-pine-moon'
-
-
 
 -- For example, changing the color scheme:
 -- config.color_scheme = 'rose-pine-moon'
@@ -28,70 +26,78 @@ config.adjust_window_size_when_changing_font_size = false
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.window_decorations = "RESIZE"
-config.font = wezterm.font { family = 'Hack Nerd Font' }
+config.font = wezterm.font({ family = "Hack Nerd Font" })
 config.font_rules = {
-  {
-    intensity = 'Bold',
-    italic = true,
-    font = wezterm.font {
-      family = 'VictorMono Nerd Font',
-      weight = 'Bold',
-      style = 'Italic',
-    },
-  },
-  {
-    italic = true,
-    intensity = 'Half',
-    font = wezterm.font {
-      family = 'VictorMono Nerd Font',
-      weight = 'DemiBold',
-      style = 'Italic',
-    },
-  },
-  {
-    italic = true,
-    intensity = 'Normal',
-    font = wezterm.font {
-      family = 'VictorMono Nerd Font',
-      style = 'Italic',
-    },
-  },
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font({
+			family = "VictorMono Nerd Font",
+			weight = "Bold",
+			style = "Italic",
+		}),
+	},
+	{
+		italic = true,
+		intensity = "Half",
+		font = wezterm.font({
+			family = "VictorMono Nerd Font",
+			weight = "DemiBold",
+			style = "Italic",
+		}),
+	},
+	{
+		italic = true,
+		intensity = "Normal",
+		font = wezterm.font({
+			family = "VictorMono Nerd Font",
+			style = "Italic",
+		}),
+	},
 }
 
 config.font_size = 17.0
 
 config.keys = {
-  { key = 'N', mods = 'CMD|SHIFT', action = wezterm.action.ToggleFullScreen },  
+	{ key = "N", mods = "CMD|SHIFT", action = wezterm.action.ToggleFullScreen },
+	{
+		key = "H",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "L",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "K",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "J",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
   {
-    key = 'H',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Left',
+    key = 'h',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
+
   {
-    key = 'L',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Right',
+    key = 'v',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
-  {
-    key = 'K',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Up',
-  },
-  {
-    key = 'J',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Down',
-  },
-  {
-    key = "w",
-    mods = "CMD",
-    action = wezterm.action.CloseCurrentPane { confirm = true },
-    },
 }
 
-
--- and finally, return the configuration to wezterm
 return config
-
