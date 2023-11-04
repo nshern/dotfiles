f4dbe989db8147ac35dc21e1b0d0fe9f97cf12f0
Niklas Alexander Shern <nashern@protonmail.com>
2023-11-04 23:16:11 +0100

    .

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,26 +1,17 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
 local config = {}
 local colors = require("lua/rose-pine-moon").colors()
 local window_frame = require("lua/rose-pine-moon").window_frame()
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
--- config.color_scheme = 'Everforest Dark (Gogh)'
--- config.color_scheme = 'Darcula (base16)'
--- config.color_scheme = 'rose-pine-moon'
-
--- For example, changing the color scheme:
--- config.color_scheme = 'rose-pine-moon'
+-- Use Rose Pine Moon Theme
 config.colors = colors
 config.window_frame = window_frame
+
 config.native_macos_fullscreen_mode = true
 config.adjust_window_size_when_changing_font_size = false
 config.tab_bar_at_bottom = false
@@ -88,14 +79,14 @@ config.keys = {
 		action = wezterm.action.CloseCurrentPane({ confirm = true }),
 	},
   {
-    key = 'h',
-    mods = 'CMD|SHIFT',
+    key = 'l',
+    mods = 'CTRL|SHIFT|CMD',
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
 
   {
-    key = 'v',
-    mods = 'CMD|SHIFT',
+    key = 'j',
+    mods = 'CTRL|SHIFT|CMD',
     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
 }
