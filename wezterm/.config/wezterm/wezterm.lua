5cb97f1c3b299523a3c25499ec75c69885f41ed1
Niklas Alexander Shern <nashern@protonmail.com>
2023-11-14 14:30:54 +0100

    clean-up

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,40 +10,43 @@ end
 
 config.colors = colors
 config.window_frame = window_frame
-config.color_scheme = "Catppuccin Frappe"
 config.native_macos_fullscreen_mode = true
 config.adjust_window_size_when_changing_font_size = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = true
+config.inactive_pane_hsb = {
+  saturation = 1.0,
+  brightness = 1.0,
+}
 config.hide_tab_bar_if_only_one_tab = false
 config.window_decorations = "RESIZE"
-config.font = wezterm.font({ family = "Hack Nerd Font" })
+config.font = wezterm.font({
+	family = "Hack Nerd Font",
+	weight = "Medium",
+	harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
+})
 config.font_rules = {
 	{
-		intensity = "Bold",
-		italic = true,
-		font = wezterm.font({
-			family = "VictorMono Nerd Font",
-			weight = "Bold",
-			style = "Italic",
-		}),
-	},
-	{
-		italic = true,
-		intensity = "Half",
-		font = wezterm.font({
-			family = "VictorMono Nerd Font",
-			weight = "DemiBold",
-			style = "Italic",
-		}),
-	},
-	{
-		italic = true,
 		intensity = "Normal",
+		italic = true,
 		font = wezterm.font({
 			family = "VictorMono Nerd Font",
+			weight = "Medium",
+			stretch = "Normal",
 			style = "Italic",
+			harfbuzz_features={ 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08' },
+		}),
+	},
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font({
+			family = "Hack Nerd Font",
+			weight = "Bold",
+			stretch = "Normal",
+			style = "Normal",
+			harfbuzz_features={ 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08' },
 		}),
 	},
 }
@@ -77,17 +80,17 @@ config.keys = {
 		mods = "CMD",
 		action = wezterm.action.CloseCurrentPane({ confirm = true }),
 	},
-  {
-    key = 'l',
-    mods = 'CTRL|SHIFT|CMD',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
+	{
+		key = "l",
+		mods = "CTRL|SHIFT|CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
 
-  {
-    key = 'j',
-    mods = 'CTRL|SHIFT|CMD',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
+	{
+		key = "j",
+		mods = "CTRL|SHIFT|CMD",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
 }
 
 return config
