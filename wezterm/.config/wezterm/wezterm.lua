1bfa35408b4e85af7cb487639a6031a158a6b269
Niklas Alexander Shern <nashern@protonmail.com>
2023-11-21 10:29:24 +0100

    feat: imporove wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,8 +8,12 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+
 config.colors = colors
 config.window_frame = window_frame
+config.window_background_opacity = 0.85
+-- config.window_background_opacity = 1.00
+config.macos_window_background_blur = 30
 config.native_macos_fullscreen_mode = true
 config.adjust_window_size_when_changing_font_size = false
 config.tab_bar_at_bottom = true
@@ -22,8 +26,10 @@ config.inactive_pane_hsb = {
 config.hide_tab_bar_if_only_one_tab = false
 config.window_decorations = "RESIZE"
 config.font = wezterm.font({
-	family = "Hack Nerd Font",
-	weight = "Medium",
+	-- family = "Hack Nerd Font",
+	-- family = "VictorMono Nerd Font",
+	family = "Inconsolata Nerd Font",
+	weight = "Regular",
 	harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
 })
 config.font_rules = {
@@ -31,7 +37,8 @@ config.font_rules = {
 		intensity = "Normal",
 		italic = true,
 		font = wezterm.font({
-			family = "VictorMono Nerd Font",
+			-- family = "VictorMono Nerd Font",
+			family = "Inconsolata Nerd Font",
 			weight = "Medium",
 			stretch = "Normal",
 			style = "Italic",
@@ -42,7 +49,8 @@ config.font_rules = {
 		intensity = "Bold",
 		italic = false,
 		font = wezterm.font({
-			family = "Hack Nerd Font",
+			family = "Inconsolata Nerd Font",
+			-- family = "Hack Nerd Font",
 			weight = "Bold",
 			stretch = "Normal",
 			style = "Normal",
