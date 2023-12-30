a2fe16bf981356366f41807ef43e8ca79c2a3214
Niklas Alexander Shern <nashern@protonmail.com>
2023-12-30 21:06:01 +0100

    add nvim

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,181 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+            lspconfig.pyright.setup({})
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		{
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.5",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			config = function()
+				local builtin = require("telescope.builtin")
+				vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+				vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+				vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+				vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+				vim.keymap.set("n", "<leader>fm", builtin.marks, {})
+			end,
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+				auto_install = true,
+				highlight = {
+					enable = true,
+				},
+
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<Leader>ss",
+						node_incremental = "<Leader>si",
+						scope_incremental = "<Leader>sc",
+						code_decremental = "<Leader>sd",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+
+						-- Automatically jump forward to textobj, similar to targets.vim
+						lookahead = true,
+
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							-- You can optionally set descriptions to the mappings (used in the desc parameter of
+							-- nvim_buf_set_keymap) which plugins like which-key display
+							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+							-- You can also use captures from other query groups like `locals.scm`
+							["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+						},
+						-- You can choose the select mode (default is charwise 'v')
+						--
+						-- Can also be a function which gets passed a table with the keys
+						-- * query_string: eg '@function.inner'
+						-- * method: eg 'v' or 'o'
+						-- and should return the mode ('v', 'V', or '<c-v>') or a table
+						-- mapping query_strings to modes.
+						selection_modes = {
+							["@parameter.outer"] = "v", -- charwise
+							["@function.outer"] = "V", -- linewise
+							["@class.outer"] = "<c-v>", -- blockwise
+						},
+						-- If you set this to `true` (default is `false`) then any textobject is
+						-- extended to include preceding or succeeding whitespace. Succeeding
+						-- whitespace has priority in order to act similarly to eg the built-in
+						-- `ap`.
+						--
+						-- Can also be a function which gets passed a table with the keys
+						-- * query_string: eg '@function.inner'
+						-- * selection_mode: eg 'v'
+						-- and should return true of false
+						include_surrounding_whitespace = true,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				vim.keymap.set("n", "<C-o>", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
+				keymaps = {
+					["g?"] = "actions.show_help",
+					["<CR>"] = "actions.select",
+					["<C-s>"] = "actions.select_vsplit",
+					["<C-h>"] = "actions.select_split",
+					["<C-t>"] = "actions.select_tab",
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = "actions.close",
+					["<C-l>"] = "actions.refresh",
+					["-"] = "actions.parent",
+					["_"] = "actions.open_cwd",
+					["`"] = "actions.cd",
+					["~"] = "actions.tcd",
+					["gs"] = "actions.change_sort",
+					["gx"] = "actions.open_external",
+					["g."] = "actions.toggle_hidden",
+					["g\\"] = "actions.toggle_trash",
+				},
+			})
+		end,
+	},
+	{
+		{
+			"echasnovski/mini.pairs",
+			version = "*",
+			config = function()
+				require("mini.pairs").setup()
+			end,
+		},
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	{
+		{
+			"echasnovski/mini.comment",
+			version = "*",
+			config = function()
+				require("mini.comment").setup()
+			end,
+		},
+	},
+})
