1bcde64528a252ca40a8960421357889c5015d90
Niklas Alexander Shern <nashern@protonmail.com>
2024-05-30 16:23:54 +0200

    plugins updates

--- nvim/.config/nvim/lua/nshern/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/nshern/plugins/lspconfig.lua
@@ -94,6 +94,7 @@ return {
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('gR', vim.lsp.buf.references, '[G]oto [R]eferences')
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
