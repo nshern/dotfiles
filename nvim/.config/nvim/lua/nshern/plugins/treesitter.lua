ae807f766ed056881fd934fdf4a950467b7a6a79
Niklas Alexander Shern <nashern@protonmail.com>
2024-05-30 16:35:34 +0200

    chore: remove comments

--- nvim/.config/nvim/lua/nshern/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/nshern/plugins/treesitter.lua
@@ -1,14 +1,11 @@
 return {
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
         ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
-        -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
@@ -23,4 +20,3 @@ return {
     end,
   },
 }
--- vim: ts=2 sts=2 sw=2 et
