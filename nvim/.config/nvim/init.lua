1e7d3b459a3432166d1901ee5b85a850757f1734
Niklas Alexander Shern <nashern@protonmail.com>
2024-06-10 21:20:41 +0200

    rm nvim

--- nvim/.config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-require 'options'
-require 'keymaps'
-require 'lazy-bootstrap'
-require 'lazy-plugins'
--- nvim/.config/nvim/lua/lazy-plugins.lua
+++
@@ -1,7 +0,0 @@
-require('lazy').setup {
-  require 'nshern/plugins/conform',
-  require 'nshern/plugins/lazygit',
-  require 'nshern/plugins/mini',
-  require 'nshern/plugins/treesitter',
-  require 'nshern/plugins/lspconfig',
-}
